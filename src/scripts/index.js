a41107c476d29d565e4f437779e9c1d09cc5eb53
Alex <alevaliev@yandex.ru>
2021-01-29 13:29:18 +0300

    added first posts pack

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,7 +1,32 @@
 import '../styles/index.scss';
+import $ from "jquery";
+import LazyLoad from 'vanilla-lazyload';
+import WOW from 'wow.js';
 
 if (process.env.NODE_ENV === 'development') {
   require('../index.pug');
 }
 
-console.log('webpack starterkit');
+$(function() {
+  var lazyLoadInstance = new LazyLoad({
+    elements_selector: '.lazy',
+  });
+
+  var wow = new WOW({
+    boxClass:     'wow',      
+    animateClass: 'animate__animated',     
+    mobile:       true,       
+    live:         true,       
+    scrollContainer: null,    
+    resetAnimation: true, 
+  });
+
+  wow.init();
+
+  $('.tabs__control').on('click', function(){
+    $('.tabs__control').removeClass('active');
+    $(this).addClass('active');
+    $('.tabs__container').hide();
+    $($(this).attr('data-target')).show();
+  });
+});
